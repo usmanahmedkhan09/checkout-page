3cd3f5017516e762cd85233d5427a06adfde3e84
usman ahmed khan <usmanahmedkhan@live.com>
2022-10-11 12:39:49 +0500

    updates in UI

---
+++ src/components/Checkout/checkout-form.vue
@@ -0,0 +1,55 @@
+<template>
+  <form class="checkout--form">
+    <div class="form--input">
+      <label for="name">CARDHOLDER NAME</label>
+      <input
+        id="name"
+        type="text"
+        placeholder="eg : Usman Ahmed"
+        autocomplete="off"
+      />
+    </div>
+    <div class="form--input">
+      <label for="card number">CARD NUMBER</label>
+      <input
+        id="card number"
+        type="text"
+        autocomplete="off"
+        placeholder="eg : 1234 6785 5634 0456"
+      />
+    </div>
+    <div class="date_wrapper">
+      <div class="form--input">
+        <label for="strat-date">EXP. DATE ( MM / YY )</label>
+        <div class="input_wrapper">
+          <input
+            id="start-date"
+            type="text"
+            autocomplete="off"
+            placeholder="MM"
+          />
+          <input
+            id="end-date"
+            type="text"
+            autocomplete="off"
+            placeholder="YY"
+          />
+        </div>
+      </div>
+      <div class="cvc form--input">
+        <label for="strat-date">CVC</label>
+        <input id="end-date" type="text" autocomplete="off" placeholder="CVC" />
+      </div>
+    </div>
+    <button class="form--button">Confirm</button>
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ src/components/Checkout/checkout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="checkout">
+    <div class="checkout--cards">
+      <div class="card--one"></div>
+      <div class="card--two"></div>
+    </div>
+    <div class="checkout--details">
+      <CheckoutForm />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import CheckoutForm from "./checkout-form.vue";
+
+export default defineComponent({
+  components: { CheckoutForm },
+  setup() {
+    return {};
+  },
+});
+</script>
